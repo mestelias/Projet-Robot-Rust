a1e2da862ce25994d993c5fb4ff55826bf19c0d6
mestelias <60406101+mestelias@users.noreply.github.com>
2020-11-07 13:55:48 +0100

    Update projet.rs

--- src/projet.rs
+++ src/projet.rs
@@ -1,4 +1,4 @@
-use std :: str :: FromStr;
+use std::str::FromStr;
 
 #[derive(Debug, PartialEq)]
 //enumeration Orientation avec les 4 cas possibles
@@ -10,52 +10,57 @@ pub enum Orientation {
 }
 
 // fonction qui lit un caractères char et renvoie une instruction
-pub fn function_instruction(instruction : char) -> Orientation {
+pub fn function_instruction(instruction: char) -> Orientation {
     match instruction {
-        'R' => Orientation :: Right,
-        'L' => Orientation :: Left,
-        'F' => Orientation ::Move,
-        _ => Orientation :: Nothing,   
+        'R' => Orientation::Right,
+        'L' => Orientation::Left,
+        'F' => Orientation::Move,
+        _ => Orientation::Nothing,
     }
 }
 
 #[derive(Debug, PartialEq)]
 // structure du robot
 pub struct Robot {
-    pub id : i32,
-    pub position_en_x : usize,
-    pub position_en_y : usize,
-    pub orientation : char,
-    pub command : Vec<char>,
+    pub id: i32,
+    pub position_en_x: usize,
+    pub position_en_y: usize,
+    pub instruction: String,
+    pub command: Vec<char>,
 }
 
 // creation d'autres robots
-fn autres_Robots() {
-    pub id :: i32,
-    pub position_en_x : usize,
-    pub position_en_y : usize,
-    pub orientation : char,
-    pub command : Vec<char>,
-} -> Robot {
-    id : id,
-    position_en_x : position_en_x,
-    position_en_y : position_en_y,
-    orientation : orientation,
-    command : command,
+fn autres_Robots(
+    id: i32,
+    position_en_x: usize,
+    position_en_y: usize,
+    instruction: String,
+    command: Vec<char>,
+) -> Robot {
+    Robot {
+        id: id,
+        position_en_x: position_en_x,
+        position_en_y: position_en_y,
+        instruction: instruction,
+        command: command,
+    }
 }
 
 // fonction qui gère les cas de collisions avec un autre robot,
 // en indiquant les coordonnées x et y de la collision
-pub fn collisions(grid : &mut Vec<Vec<char>>, robot_vecteur : &Vec<Robot>) {
-    for i in 0.. robot_vecteur.len() {
-        for j in 0.. robot_vecteur.len() {
-            if (robot_vecteur[i].id != robot_vecteur[j].id) 
-            && (robot_vecteur[i].position_en_x == robot_vecteur[j].position_en_x)
-            && (robot_vecteur[i].position_en_y == robot_vecteur[j].position_en_y)
+pub fn collisions(grid: &mut Vec<Vec<char>>, robot_vecteur: &Vec<Robot>) {
+    for i in 0..robot_vecteur.len() {
+        for j in 0..robot_vecteur.len() {
+            if (robot_vecteur[i].id != robot_vecteur[j].id)
+                && (robot_vecteur[i].position_en_x == robot_vecteur[j].position_en_x)
+                && (robot_vecteur[i].position_en_y == robot_vecteur[j].position_en_y)
             {
-                println!("Robot ID<{}> Collision en ({}, {})",
-                robot_vecteur[i].id, robot_vecteur[i].position_en_x, robot_vecteur[i].position_en_y
-                ); 
+                println!(
+                    "Robot ID<{}> Collision en ({}, {})",
+                    robot_vecteur[i].id,
+                    robot_vecteur[i].position_en_x,
+                    robot_vecteur[i].position_en_y
+                );
             }
         }
     }
