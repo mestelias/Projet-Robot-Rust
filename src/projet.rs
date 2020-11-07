49845822e749d89a67ed58a1ae313a3f95d2f175
mestelias <60406101+mestelias@users.noreply.github.com>
2020-11-07 17:32:22 +0100

    Update projet.rs

--- src/projet.rs
+++ src/projet.rs
@@ -1,7 +1,7 @@
 use std::str::FromStr;
 
 #[derive(Debug, PartialEq)]
-//enumeration Orientation avec les 4 cas possibles
+// enumeration Orientation avec les 4 cas possibles
 pub enum Orientation {
     Right,
     Left,
@@ -33,7 +33,7 @@ pub struct Robot {
     pub command: Vec<char>,
 }
 
-// creation d'autres robots
+// création d'autres robots
 fn autres_Robots(
     id: i32,
     position_en_x: usize,
@@ -50,6 +50,7 @@ fn autres_Robots(
     }
 }
 
+//REMIX: On a rajouté les cas de collisions avec les bordures du plateau
 // fonction qui gère les cas de collisions avec un autre robot et affiche le plateau,
 // en indiquant les coordonnées x et y de la collision
 pub fn collisions(
@@ -59,15 +60,15 @@ pub fn collisions(
 ) {
     for i in 0..robot_vecteur.len() {
         for j in 0..robot_vecteur.len() {
-            //on verifie d'abord que les robots ne sont pas les memes
+            //on vérifie d'abord que les robots ne sont pas les memes
             if (robot_vecteur[i].id != robot_vecteur[j].id)
-                //Puis on verifie les coordonnees en x et en y
+                //Puis on vérifie les coordonnées en x et en y
                 && (robot_vecteur[i].position_en_x == robot_vecteur[j].position_en_x)
                 && (robot_vecteur[i].position_en_y == robot_vecteur[j].position_en_y)
-                //Puis verifie si le premier robot tape les extremites
+                //Puis vérifie si le premier robot tape les extrémites
                 && (robot_vecteur[i].position_en_x == grid[MIN][MAX])
                 && (robot_vecteur[i].position_en_y == grid[MIN][MAX])^
-                //Puis verifie si le deuxieme robot tape les extremites
+                //Puis vérifie si le deuxième robot tape les extrémites
                 && (robot_vecteur[j].position_en_x == grid[MIN][MAX])
                 && (robot_vecteur[j].position_en_y == grid[MIN][MAX])
             {
@@ -80,7 +81,7 @@ pub fn collisions(
             }
         }
     }
-    // Affiche le plateau
+    // affiche le plateau
     for i in 0..plateau_size[0] {
         for j in 0..plateau_size[1] {
             print!("{}", grid[i][j]);
@@ -123,7 +124,7 @@ mod tests {
     }
 
     #[test]
-    // Creation des robots
+    // creation des robots
     fn test_autres_Robots() {
         let a = autres_Robots(1, 'r'.to_string(), 'r', 1, 1, vec!['R', 'L', 'F']);
         let robot_test: Robot = Robot {
